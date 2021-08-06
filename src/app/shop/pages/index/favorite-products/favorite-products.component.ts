341cdd59dc761665e03c9911666ff3f3c5f5a0d3
mohammadmatloub <mohmmadmatloub20001379@gmail.com>
2021-08-06 22:46:30 +0430

    00-05-15

---
+++ src/app/shop/pages/index/favorite-products/favorite-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'favorite-products',
+  templateUrl: './favorite-products.component.html',
+  styleUrls: ['./favorite-products.component.css']
+})
+export class FavoriteProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shop/pages/index/special-products/special-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'special-products',
+  templateUrl: './special-products.component.html',
+  styleUrls: ['./special-products.component.css']
+})
+export class SpecialProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
